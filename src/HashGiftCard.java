a687846e09fb300055c01f745798e9e90fd65ec0
rmr84 <73509790+rmr84@users.noreply.github.com>
2021-03-25 16:53:51 -0400

    chris weld

--- src/HashGiftCard.java
+++ src/HashGiftCard.java
@@ -2,25 +2,24 @@ import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 
-@SuppressWarnings("hiding")
-	public class HashGiftCard<User, ArrayList<GiftCard>> implements Dictionary<User,  ArrayList<GiftCard>> {
+
+	public class HashGiftCard<User, ArrayList<GiftCard> {
 		private int size; 
 		private Object[] entries;
 		
-		public HashGiftCard(int initialCapacity) {
-			entries = new Object[initialCapacity];
-			size = 0;
+		public HashGiftCard() {
+		
 		}
 
 
 		public Iterator<User> keys() {
-		// TODO Auto-generated method stub
+		
 			return null;
 		}
 
 	
 		public Iterator<ArrayList> elements() {
-		// TODO Auto-generated method stub
+		
 			return null;
 		}
 
@@ -32,7 +31,7 @@ import java.util.NoSuchElementException;
 
 	
 		public V remove(User Userey) {
-		// TODO Auto-generated method stub
+		
 			return null;
 		}
 
